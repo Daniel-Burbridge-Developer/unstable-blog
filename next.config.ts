351b46489fc68e704dc00b4491542b13d9a03897
daniel-burbridge-developer <daniel.burbridge.developer@gmail.com>
2025-02-03 23:05:26 +0800

    ts expect error

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
 
 const withMDX = createMDX({
   options: {
-    // eslint-disable-next-line
+    // @ts-expect-error
     remarkPlugins: [["remark-gfm", { strict: true, throwOnError: true }]],
     rehypePlugins: [],
   },
