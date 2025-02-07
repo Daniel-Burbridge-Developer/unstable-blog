36cf230d82925c61e6b5e867caed5ee7f623be10
daniel-burbridge-developer <daniel.burbridge.developer@gmail.com>
2025-02-07 15:41:14 +0800

    prototyping clerk

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,22 @@
 import { SignOutButton } from '@clerk/nextjs';
-import { auth } from '@clerk/nextjs/server';
+import { auth, currentUser } from '@clerk/nextjs/server';
 
 const adminPage = async () => {
   const { userId, redirectToSignIn } = await auth();
+  const user = await currentUser();
 
   if (!userId) return redirectToSignIn();
 
   return (
     <div>
       <h1>Hello There</h1> <SignOutButton />
+      <h1>
+        {(user?.publicMetadata.roles as string[]).some(
+          (role) => role === 'admin'
+        )
+          ? 'Hello admin'
+          : null}
+      </h1>
     </div>
   );
 };
