3a8023fee14d9218a65601a787ab7fd8e5d5d9c6
daniel-burbridge-developer <daniel.burbridge.developer@gmail.com>
2025-02-09 13:55:31 +0800

    getting files

--- app/admin/downloader/page.tsx
+++ app/admin/downloader/page.tsx
@@ -2,11 +2,18 @@ import { utapi } from '@/server/uploadthing';
 
 const Home = async () => {
   const files = await utapi.listFiles();
-  console.log(files);
+  const fileKeys = files.files.map((file) => file.key);
+  const BASEURL = 'https://m5vq5hwl5l.ufs.sh/f/';
 
   return (
-    <div>
+    <div className="text-white">
       <h1>FILES:</h1>
+      <br />
+      <ul className="flex gap-4 flex-col">
+        {files.files.map((file) => (
+          <li key={file.key}>{`${BASEURL}${file.key}`}</li>
+        ))}
+      </ul>
     </div>
   );
 };
